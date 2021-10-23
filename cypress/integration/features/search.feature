13934ebbf8ab3df34d17e0dc96c69fb054e867ec
qabuffalo <92962310+qabuffalo@users.noreply.github.com>
2021-10-22 21:56:26 -0400

    please push

--- cypress/integration/features/search.feature
+++ cypress/integration/features/search.feature
@@ -16,4 +16,25 @@ Feature: target
             | Shoes   |
             | Pants   |
 
+<<<<<<< Updated upstream:cypress/integration/features/search.feature
+=======
+    Scenario: Click on categories tab
+        Given I am in Tagret Home page
+        When I click categories tab
+        Then I click on Main Menu option
+
+    Scenario: Login
+        Given I am in Tagret Home page
+        When I click login
+        And I click on Sign-in button
+        Then I will be taken to to Sign-in page
+        And I will enter username
+        And I will enter password
+        Then I will click Enter
+
+
+    Scenario: Create account
+        Given I am in Tagret Home page
+        When I click create account
+>>>>>>> Stashed changes:cypress/integration/features/target.feature
         
--- cypress/support/step_definitions/target_steps.js
+++ cypress/support/step_definitions/target_steps.js
@@ -1,6 +1,10 @@
-/// <reference types="cypress" />
-
+/// <reference types= "cypress" /> 
 import { Given } from "cypress-cucumber-preprocessor/steps"
+<<<<<<< Updated upstream
+=======
+import { target } from "/Users/asadzaman/Documents/GitHub/cypress/cypress/integration/pages/target.js"
+
+>>>>>>> Stashed changes
 
 Given('I visit target home page', () => {
     cy.visit('/')
@@ -33,3 +37,27 @@ Then('I click on Main Menu option', () => {
     cy.contains('Main Menu').click()
 }) 
 
+When('I click login', () => {
+    cy.get(target.Login).click()
+})
+
+And('I click on Sign-in button', () => {
+    cy.get(target,Sign_In).click()
+})
+
+
+
+Then('I will be taken to to Sign-in page', () => {
+    cy.title().should('contain', 'Login: Target')
+})
+
+And('I will enter username', () => {
+    cy.get(target.Username).type("qabuffalo@gmail.com")
+})
+And('I will enter password', () => {
+    cy.get(target.Password).type('qabuffalo')
+})
+
+Then('I will click Enter', () => {
+    cy.type('{enter}')
+})
